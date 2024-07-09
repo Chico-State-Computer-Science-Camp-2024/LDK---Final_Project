449ebe4e0d797d2e40bcab029f79693af91c7d1d
Gansito0 <sotomaximiliano310726@gmail.com>
2024-07-09 17:08:29 +0000

    moveable square

--- js/main.js
+++ js/main.js
@@ -1,10 +1,38 @@
+var X = 200;
+var Y = 200;
 
-function setup() {
-  createCanvas(500, 300);
+function setup() { 
+  createCanvas(400, 400);
+} 
 
+function draw() { 
+  background(0);
+  square(X, Y, 20);
+  
+  if (keyIsPressed) {
+    if(key === "a"){
+      X -= 5;
+    } else if (key === "d"){
+      X += 5;
+    } else if (key === "w"){
+      Y -= 5;
+    } else if (key === "s"){
+      Y += 5;
+    }
   }
-
-function draw() {
-    background(0);
-
 }
+
+
+
+
+
+/*if (keyCode == RIGHT_ARROW) {
+    X +=5; 
+   }  else if (keyCode == LEFT_ARROW) {
+     X -= 5;
+   } else if (keyCode == UP_ARROW) {
+     Y -= 5; 
+   } else if (keyCode == DOWN_ARROW) {
+     Y +=5; 
+   }
+  } */
