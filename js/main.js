b8ebfbdd38475faf1d2e391320f9f008acd233f7
Gansito0 <sotomaximiliano310726@gmail.com>
2024-07-10 21:16:28 +0000

    snake code

--- js/main.js
+++ js/main.js
@@ -1,23 +1,4 @@
 
-<<<<<<< HEAD
-        function myFunction() {
-          document.getElementById("myDropdown").classList.toggle("show");
-        }
-        
-        // Close the dropdown if the user clicks outside of it
-        window.onclick = function(event) {
-          if (!event.target.matches('.dropbtn')) {
-            var dropdowns = document.getElementsByClassName("dropdown-content");
-            var i;
-            for (i = 0; i < dropdowns.length; i++) {
-              var openDropdown = dropdowns[i];
-              if (openDropdown.classList.contains('show')) {
-                openDropdown.classList.remove('show');
-              }
-            }
-          }
-        }
-=======
 let Y = 200;
 let X = 200;
 
@@ -45,4 +26,3 @@ function draw() {
 function snakecolor() {
   
 }
->>>>>>> main
