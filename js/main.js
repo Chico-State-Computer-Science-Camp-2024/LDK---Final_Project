5297f04da459a642cbf5f596c0ac6a02306eabe8
Kai <kgoodman1@csuchico.edu>
2024-07-10 18:18:20 +0000

    kai stuff

--- js/main.js
+++ js/main.js
@@ -1,31 +1,4 @@
 
-let Y = 200;
-let X = 200;
-
-function setup() { 
-  createCanvas(400, 400);
-} 
-
-function draw() { 
-  background(0);
-  square(X, Y, 20);
-  
-  if (keyIsPressed) {
-    if(key === "a"){
-      X -= 5;
-    } else if (key === "d"){
-      X += 5;
-    } else if (key === "w"){
-      Y -= 5;
-    } else if (key === "s"){
-      Y += 5;
-    }
-  }
-}
-
-
-/* When the user clicks on the button, 
-        toggle between hiding and showing the dropdown content */
         function myFunction() {
           document.getElementById("myDropdown").classList.toggle("show");
         }
